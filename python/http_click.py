f63ae36eecbc65720fa361a6f58d3991fda95bb1
Linfei Gui <linfei.gui@LinfeideMacBook-Pro.local>
2018-09-04 19:02:35 +0800

    new http

---
+++ python/http_click.py
@@ -0,0 +1,10 @@
+#!/Library/Frameworks/Python.framework/Versions/3.7/bin/python3
+
+from selenium import webdriver 
+
+import os  
+
+
+driver = webdriver.Chrome()
+driver.get("www.baidu.com")
+
--- python/par_know.py
+++ python/par_know.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+
+def power(x):
+    return x*x 
+
+def power1(x,n=2): 
+    s=1
+    while n>0:
+        n=n-1
+        s=s*x
+    return s
+
+if __name__=="__main__":
+    print("f1\n")
+    print(power(3))
+    print(power1(3))
+
+    print(power1(3,n=4))
+    print(power1(3))
